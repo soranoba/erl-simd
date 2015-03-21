6f6a5bcd14b8c2f3c87f039b6f2165c3caf557d1
soranoba <soranoba@gmail.com>
2015-03-21 12:49:46 +0900

    NIF test.

---
+++ c_src/simd.d
@@ -0,0 +1,5 @@
+c_src/simd.o: c_src/simd.c /usr/lib/erlang/erts-6.3/include/erl_nif.h \
+ /usr/lib/erlang/erts-6.3/include/erl_native_features_config.h \
+ /usr/lib/erlang/erts-6.3/include/erl_drv_nif.h \
+ /usr/lib/erlang/erts-6.3/include/erl_int_sizes_config.h \
+ /usr/lib/erlang/erts-6.3/include/erl_nif_api_funcs.h
---
+++ src/simd.erl
@@ -0,0 +1,19 @@
+-module(simd).
+-export([foo/1, bar/1, get_priv/0, init/0]).
+-on_load(init/0).
+
+init() ->
+    SoName = filename:join([get_priv(), ?MODULE]),
+    ok = erlang:load_nif(SoName, 0).
+
+foo(_X) ->
+    exit(nif_library_not_loaded).
+
+bar(_Y) ->
+    exit(nif_library_not_loaded).
+
+%% @doc Get the priv path of wslogi.
+-spec get_priv() -> file:filename().
+get_priv() ->
+    {ok, Priv} = moyo_application:get_priv_dir(simd),
+    Priv.
